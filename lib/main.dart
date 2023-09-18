fd51c61691cd0e9c2c5bec92c33035be56052416
rai-rmg <rony.abelli@r-mab.com>
2023-09-18 16:02:21 -0400

    adding width and height parameter

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -90,7 +92,8 @@ class DynamicFormPage extends StatelessWidget {
       padding: const EdgeInsets.all(16.0),
       child: Column(
         children: [
-          DynamicFormWidget(formFields: getFormFields()),
+          DynamicFormWidget(
+              height: 520.0, width: 300.0, formFields: getFormFields()),
         ],
       ),
     );
@@ -99,8 +102,11 @@ class DynamicFormPage extends StatelessWidget {
 
 class DynamicFormWidget extends StatefulWidget {
   final List<Map<String, dynamic>> formFields;
+  final double? width;
+  final double? height;
 
-  DynamicFormWidget({required this.formFields});
+  const DynamicFormWidget(
+      {super.key, required this.formFields, required this.width, required this.height});
 
   @override
   _DynamicFormWidgetState createState() => _DynamicFormWidgetState();
@@ -108,144 +114,157 @@ class DynamicFormWidget extends StatefulWidget {
 
 class _DynamicFormWidgetState extends State<DynamicFormWidget> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  Map<String, dynamic> _formData = {};
+  final Map<String, dynamic> _formData = {};
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          ...getFormFields().map((field) {
-            final id = field['id'].toString();
-            final question = field['question'] as String;
-            final answerType = field['answer_type'] as String;
-            final answerFormat = field['answer_format'] as String;
+    return SizedBox(
+      width: widget.width ?? 400,
+      height: widget.height ?? 300,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical, // Utilisez le défilement vertical
+        physics:
+           const AlwaysScrollableScrollPhysics(), // Autorisez le défilement même si le contenu s'adapte
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              ...getFormFields().map((field) {
+                final id = field['id'].toString();
+                final question = field['question'] as String;
+                final answerType = field['answer_type'] as String;
+                final answerFormat = field['answer_format'] as String;
 
-            Widget inputWidget = Text('Initial Widget');
-            if (answerType == 'text') {
-              inputWidget = TextFormField(
-                decoration: InputDecoration(labelText: question),
-                validator: (value) {
-                  final regexp = RegExp(answerFormat);
-                  if (value == null || !regexp.hasMatch(value)) {
-                    return 'Format invalide';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _formData[id] = value;
-                },
-              );
-            } else if (answerType == 'radio') {
-              // Gérer le champ de type radio
-              final radioOptions =
-                  field['answer_available_values'] as List<dynamic>;
+                Widget inputWidget = const Text('Initial Widget');
+                if (answerType == 'text') {
+                  inputWidget = TextFormField(
+                    decoration: InputDecoration(labelText: question),
+                    validator: (value) {
+                      final regexp = RegExp(answerFormat);
+                      if (value == null || !regexp.hasMatch(value)) {
+                        return 'Format invalide';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _formData[id] = value;
+                    },
+                  );
+                } else if (answerType == 'radio') {
+                  // Gérer le champ de type radio
+                  final radioOptions =
+                      field['answer_available_values'] as List<dynamic>;
 
-              inputWidget = Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: radioOptions.map((option) {
-                  final radioLabel = option['label']!;
-                  final radioValue = option['value']!;
-                  return Row(
+                  inputWidget = Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: radioOptions.map((option) {
+                      final radioLabel = option['label']!;
+                      final radioValue = option['value']!;
+                      return Row(
+                        children: [
+                          Radio(
+                            value: radioValue,
+                            groupValue: _formData[id],
+                            onChanged: (value) {
+                              setState(() {
+                                _formData[id] = value;
+                              });
+                            },
+                          ),
+                          Text(radioLabel),
+                        ],
+                      );
+                    }).toList(),
+                  );
+                } else if (answerType == 'select') {
+                  final selectOptions =
+                      field['answer_available_values'] as List<dynamic>;
+
+                  inputWidget = SingleChildScrollView(
+                    child: DropdownButtonFormField<String>(
+                      value: _formData[id],
+                      items:
+                          selectOptions.map<DropdownMenuItem<String>>((option) {
+                        final selectLabel = option['value']!;
+                        final selectValue = option['label']!;
+                        return DropdownMenuItem<String>(
+                          value: selectValue,
+                          child: Text(selectLabel),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          _formData[id] = value;
+                        });
+                      },
+                      decoration: InputDecoration(labelText: question),
+                      validator: (value) {
+                        if (value == null) {
+                          return 'Veuillez sélectionner une option';
+                        }
+                        return null;
+                      },
+                    ),
+                  );
+                } else if (answerType == 'boolean') {
+                  final checkboxOptions =
+                      field['answer_available_values'] as String;
+
+                  inputWidget = Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Radio(
-                        value: radioValue,
-                        groupValue: _formData[id],
-                        onChanged: (value) {
-                          setState(() {
-                            _formData[id] = value;
-                          });
-                        },
-                      ),
-                      Text(radioLabel),
+                      Text(question,
+                          style: const TextStyle(fontWeight: FontWeight.bold)),
+                      ...checkboxOptions.split(' ou ').map<Widget>((option) {
+                        return Row(
+                          children: [
+                            Checkbox(
+                              value: _formData[id]?.contains(option) ?? false,
+                              onChanged: (value) {
+                                setState(() {
+                                  if (_formData[id] == null) {
+                                    _formData[id] = [];
+                                  }
+                                  if (value == true) {
+                                    _formData[id]!.add(option);
+                                  } else {
+                                    _formData[id]!.remove(option);
+                                  }
+                                });
+                              },
+                            ),
+                            Text(option),
+                          ],
+                        );
+                      }).toList(),
                     ],
                   );
-                }).toList(),
-              );
-            } else if (answerType == 'select') {
-              final selectOptions =
-                  field['answer_available_values'] as List<dynamic>;
+                } else {
+                  // Type de champ non pris en charge
+                  inputWidget = const Text('Type de champ non pris en charge');
+                }
 
-              inputWidget = DropdownButtonFormField<String>(
-                value: _formData[id],
-                items: selectOptions.map<DropdownMenuItem<String>>((option) {
-                  final selectLabel = option['value']!;
-                  final selectValue = option['label']!;
-                  return DropdownMenuItem<String>(
-                    value: selectValue,
-                    child: Text(selectLabel),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _formData[id] = value;
-                  });
-                },
-                decoration: InputDecoration(labelText: question),
-                validator: (value) {
-                  if (value == null) {
-                    return 'Veuillez sélectionner une option';
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: inputWidget,
+                );
+              }).toList(),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    final jsonString = jsonEncode(_formData);
+                    print(jsonString);
+                    // Faites ce que vous voulez avec le JSON généré, comme l'envoyer à un serveur ou le stocker localement.
                   }
-                  return null;
                 },
-              );
-            } else if (answerType == 'boolean') {
-              final checkboxOptions =
-                  field['answer_available_values'] as String;
-
-              inputWidget = Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(question, style: TextStyle(fontWeight: FontWeight.bold)),
-                  ...checkboxOptions.split(' ou ').map<Widget>((option) {
-                    return Row(
-                      children: [
-                        Checkbox(
-                          value: _formData[id]?.contains(option) ?? false,
-                          onChanged: (value) {
-                            setState(() {
-                              if (_formData[id] == null) {
-                                _formData[id] = [];
-                              }
-                              if (value == true) {
-                                _formData[id]!.add(option);
-                              } else {
-                                _formData[id]!.remove(option);
-                              }
-                            });
-                          },
-                        ),
-                        Text(option),
-                      ],
-                    );
-                  }).toList(),
-                ],
-              );
-            } else {
-              // Type de champ non pris en charge
-              inputWidget = const Text('Type de champ non pris en charge');
-            }
-
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: inputWidget,
-            );
-          }).toList(),
-          ElevatedButton(
-            onPressed: () {
-              if (_formKey.currentState!.validate()) {
-                _formKey.currentState!.save();
-                final jsonString = jsonEncode(_formData);
-                print(jsonString);
-                // Faites ce que vous voulez avec le JSON généré, comme l'envoyer à un serveur ou le stocker localement.
-              }
-            },
-            child: Text('Submit'),
-          ), // Fin du bouton Submit
-        ], // Fin de la colonne des champs
-      ), // Fin du formulaire
-    ); // Fin du widget Form
+                child: const Text('Submit'),
+              ), // Fin du bouton Submit
+            ], // Fin de la colonne des champs
+          ), // Fin du formulaire
+        ), // Fin du widget Form
+      ), // Fin du widget SingleChildScrollView
+    ); // Fin du widget Container
   }
 } // Fin du widget DynamicFormWidget
 
